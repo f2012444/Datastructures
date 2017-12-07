34ec82f7fd10f0dac07a4f8aad8b06062254a82c
f2012444 <venkateshmagham21@gmail.com>
2017-12-06 21:45:30 -0700

    Added LinkedList and Node

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,2 +1,89 @@
-public class LinkedList {
+public class LinkedList implements List {
+
+    int size;
+    Node root;
+
+    public LinkedList() {
+        this.root = null;
+    }
+    public LinkedList(Node root) {
+        this.root = root;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void add(Object o) {
+        if(root == null) {
+            root = (Node)o;
+            root.next = null;
+
+        }
+        Node temp = root;
+        while(temp.next!=null) {
+            temp = temp.next;
+        }
+        temp.next = (Node)o;
+        size++;
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if(size == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object O) {
+        Node temp = root;
+        while(temp!=null) {
+            if(temp.equals(O)) {
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
+
+    @Override
+    public Object get(int index) {
+
+        return null;
+    }
+
+    @Override
+    public void remove(Object O) {
+        if(root.equals(null)) {
+            return;
+        }
+        if(root.equals(O)) {
+            root = root.next;
+        }
+        Node temp = root;
+        Node prev = root;
+        while(temp!=null) {
+            if(temp.equals(O)) {
+                prev.next = temp.next;
+            }
+            prev = temp;
+            temp = temp.next;
+        }
+
+    }
+
+    @Override
+    public int indexOf(Object O) {
+        return 0;
+    }
+
+    @Override
+    public void remove(int index) {
+
+    }
 }
---
+++ src/Node.java
@@ -0,0 +1,12 @@
+public class Node {
+    int value;
+    Node next;
+    public Node(int value) {
+        this.value = value;
+        next = null;
+    }
+    public Node(int value, Node next) {
+        this.value = value;
+        this.next = next;
+    }
+}
